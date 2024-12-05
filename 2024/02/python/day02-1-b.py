4a1a54aee0bcfc111ed8080a915625c5fc31a32b
mosaic@TEQUILA <mark.j@usask.ca>
2024-12-05 00:54:32 -0600

    instead of onto list, use combination for part 2

--- 2024/02/python/day02-1-b.py
+++ 2024/02/python/day02-1-b.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from functools import reduce
+from itertools import combinations
 
 def is_strictly_increasing(lst):
     if not lst:  # Handle empty list case
@@ -18,4 +19,24 @@ def is_strictly_decreasing(lst):
                       (x[0] > x[1]) and 
                       (x[0]-x[1] <=3 ), 
                   zip(lst, lst[1:]), True)
+
+n=int(input())
+
+c=0
+for _ in range(n):
+    l = list(map(int, input().split()))
+    if (is_strictly_decreasing(l) or 
+        is_strictly_increasing(l) or
+        reduce(lambda acc, x: acc or x,
+               map( lambda l:
+                        is_strictly_increasing(l) or
+                        is_strictly_decreasing(l),
+                   combinations(l, len(l)-1)
+                   )
+               )
+        ):
+        c+=1
+
+print(c)
+
 
