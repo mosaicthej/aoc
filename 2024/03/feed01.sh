68d2826c5fd91897a92c20682018fc00789a1e61
mosaic@TEQUILA <mark.j@usask.ca>
2024-12-05 00:57:20 -0600

    sed to clean up,

---
+++ 2024/03/feed01.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# expecting 'cat input'
+(sed 's/\(mul([0-9]\+,[0-9]\+)\)/\n\1\n/g' | grep 'mul([0-9]\+,[0-9]\+)' | sed 's/\mul(\([0-9]*\),\([0-9]*\))/\1*\2/' && echo '') | ./adder.py 
