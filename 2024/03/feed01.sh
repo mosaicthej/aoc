a29373166a50b5c562459d7bce26ce6b02ba7bd9
mosaic@TEQUILA <mark.j@usask.ca>
2024-12-05 20:13:21 -0600

    feed01

--- 2024/03/feed01.sh
+++ 2024/03/feed01.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
 # expecting 'cat input'
-(sed 's/\(mul([0-9]\+,[0-9]\+)\)/\n\1\n/g' | grep 'mul([0-9]\+,[0-9]\+)' | sed 's/\mul(\([0-9]*\),\([0-9]*\))/\1*\2/' && echo '') | ./adder.py 
+(sed 's/\(mul([0-9]\+,[0-9]\+)\)/\n\1\n/g' \
+    | grep 'mul([0-9]\+,[0-9]\+)' \
+    | sed 's/\mul(\([0-9]*\),\([0-9]*\))/\1*\2/' && echo '') \
+    | ./adder.py 
