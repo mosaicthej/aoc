297b977cddc15525ce915cf6ad2f117dd6cfcc25
mosaic@TEQUILA <mark.j@usask.ca>
2024-12-07 09:02:33 -0600

    should have done this all purpose stackmachine

--- 2024/07/python/solve-2.py
+++ 2024/07/python/solve-2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import itertools, operator
+from typing import List
 
 n=int(input())
 
@@ -16,6 +17,20 @@ def leftjoin(e1:str, e2:str) -> str: # '22+', '32*'; '(22+32)+', '67+'
             ')' +
             (e2[-1] if e2[-1] in '+*' else ''))
 
+
+def stackmachine(accu:int, last_op, # init with: accu=0, op=+
+                 rest:tuple[tuple[int,int]]): # ((1,'+'), (3,'|'), (3,None))
+    if not rest:
+        return accu # base case, when done
+    else: 
+        if last_op == '+':
+            ac = accu+rest[0][0]
+        elif last_op == '*':
+            ac = accu*rest[0][0]
+        else: # last_op == '|':
+            ac = int(str(accu)+str(rest[0][0]))
+        return stackmachine(ac, rest[0][1], rest[1:])
+
 for _ in range(n):
     exps = []
     r,ex = input().split(': ')
@@ -27,30 +42,12 @@ for _ in range(n):
        #     )) for os in oss]
 
     for os in oss:
-        build = ''
-        exp = tuple( map(lambda x: operator.add(x[0],x[1]) if x[1] else x[0],
-                itertools.zip_longest(nus, os))) # ('3+', '5*', '66|', '5')
-        
-        expl = [];i=0
-        while(i<len(exp)):
-            if i<len(exp) and len(expl)>0 and expl[-1][-1]=='|':
-                # if the building list ends with |
-                expl[-1] = expl[-1][:-1] + exp[i]
-                i += 1 # exp[i] would be next
-            else:
-                e = exp[i] # on ith thing
-                expl.append(e)
-                i+=1
-
-        print(exp,expl)
-        for es in expl:
-            build = leftjoin(build, es)
-
-        res = eval(build)
+        t0 = tuple(itertools.zip_longest(map(int,nus), os))
+        res = stackmachine(0,'+',t0)
         if res==r:
-           s+=res
-           print('happy')
-           break
+            print(t0)
+            s+=res
+            break
 print(s)
 
 
