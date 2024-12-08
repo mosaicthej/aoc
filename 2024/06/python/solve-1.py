1c0c11f2ae3ef25acfd4c376baf1abb986874c51
mosaic@TEQUILA <mark.j@usask.ca>
2024-12-08 16:51:23 -0600

    tail rec

--- 2024/06/python/solve-1.py
+++ 2024/06/python/solve-1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from typing import Optional
 import operator
-from tail_recursion import tail_recursive
 
 
 n=int(input())
@@ -35,19 +34,51 @@ padd = lambda p,d: tuple(map(operator.add, p, d)) # position adder
 
 guard_currpos = findGuard(mat, 0, '^')
 
+import sys
+
+class TailRecurseException(Exception):  # Update to inherit from Exception
+    def __init__(self, args, kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+def tail_call_optimized(g):
+    """
+    This function decorates a function with tail call
+    optimization. It does this by throwing an exception
+    if it is its own grandparent, and catching such
+    exceptions to fake the tail call optimization.
+    
+    This function fails if the decorated
+    function recurses in a non-tail context.
+    """
+    def func(*args, **kwargs):
+        f = sys._getframe()
+        if f.f_back and f.f_back.f_back \
+                and f.f_back.f_back.f_code == f.f_code:
+            raise TailRecurseException(args, kwargs)
+        else:
+            while True:  # Replace 1 with True for better readability
+                try:
+                    return g(*args, **kwargs)
+                except TailRecurseException as e:  # Update exception syntax
+                    args = e.args
+                    kwargs = e.kwargs
+    func.__doc__ = g.__doc__
+    return func
+
+
 '''states to track:
     - the entire map
     - the current location of guard
     - the mode of the guard
 returns the state for next step'''
-@tail_recursive
+@tail_call_optimized
 def nextState(mat:list[str], gloc:Pos, mode:str) -> list[str]:
     gr,gc = gloc
     if (gr < 0 or gr >= n or gc < 0 or gc >= len(mat[0])): return mat
 
     rmat, nmod = mat, mode
     nr,nc = padd(gloc, gm2d(mode)) # new loc for guard
-    print(nr, nr<n)
     if not (nr >= 0 and nr < n and nc >= 0 and nc < len(mat[0])): # walked out
         rmat[gr] = mat[gr][:gc] + 'X' + mat[gr][gc+1:] # mark the current
         return rmat # return the map
@@ -61,7 +92,6 @@ def nextState(mat:list[str], gloc:Pos, mode:str) -> list[str]:
         rmat[nr] = mat[nr][:nc] + nmod + mat[nr][nc+1:]
 
     # done update, call next move
-    print(rmat)
     return nextState(rmat, (nr,nc), nmod)
 
 
