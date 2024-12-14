52be14b9fd5e9641ed8ae1995802a8fe3234ac09
mosaic@TEQUILA <mark.j@usask.ca>
2024-12-14 12:00:04 -0600

    start

---
+++ 2024/14/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+# run through the input file,
+# count the number of lines, 
+# and return num lines as well as inputs
+
+wd=$(dirname "$0")
+n=$(wc $wd/input -l  | awk '{ print $1 }')
+echo $n
+cat $wd/input
